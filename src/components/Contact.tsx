b5bc912f73ed91a72fd2d957dc6fac993f878819
PydevG <peterndindi41@gmail.com>
2025-05-26 15:22:16 +0300

    cors

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -21,7 +21,7 @@ const handleSubmit = async (e: React.FormEvent) => {
   };
 
   try {
-    const response = await fetch("http://localhost:5000/api/contact", {
+    const response = await fetch("https://https://nexus-code-studio.onrender.com/api/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
